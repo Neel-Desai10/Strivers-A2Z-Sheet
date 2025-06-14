8f3cac0366f19221da99e70a58ba0e73eefa3985
Neel Desai <cr7nd7@gmail.com>
2025-06-14 10:53:22 +0530

    User Input/Output

---
+++ Strivers_A2Z_Sheet.py
@@ -0,0 +1,8 @@
+#Input Output
+#Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+class Solution:
+    def printNumber():
+        x = input("Enter a number: ")
+        print(int(x))
+    printNumber()
+
